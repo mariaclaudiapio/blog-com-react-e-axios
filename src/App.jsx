372aa179bb238782ad4213414a5694d1a0a06593
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-26 16:25:20 -0300

    criação do componente 'barra de navegação'

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 
+import Navbar from "./components/navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="container">
         <Outlet />
       </div>
