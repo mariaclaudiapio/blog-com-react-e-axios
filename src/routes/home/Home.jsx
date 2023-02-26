5324c3854733d0dfef66c33182e130285ac8e57d
mariaclaudiapio <mariaclaudiapio@gmail.com>
2023-02-26 17:16:18 -0300

    resgate e exibição do conteúdo da API no componente 'Home'

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,7 +1,46 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+import "./Home.css";
+
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = response.data;
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
-      <h2>Home</h2>
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   );
 };
